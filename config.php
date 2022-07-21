3610f92523a2591d2d1cb3aa61f59eca5573a87d
Deep Rakesh <deeptank09@gmail.com>
2022-07-21 22:11:26 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,25 +1,27 @@
 <?php 
 
-//Development connection
-// $server="localhost";
-// $username="root";
-// $password="";
-// $db="Bank Database";
+// Development connection
+$server="localhost";
+$username="root";
+$password="";
+$db="Bank Database";
 
 //Remote database connection
-$server="remotemysql.com";
-$username="fZSeld98gz";
-$password=" ubDqvVvLZs";
-$db="fZSeld98gz";
+// $server="remotemysql.com";
+// $username="TMYDPECOlQ";
+// $password=" 6a3cppv34D";
+// $db="TMYDPECOlQ";
 
-$conn=mysqli_connect($server,$username,$password,$db);
+$conn=mysqli_connect("remotemysql.com","TMYDPECOlQ","6a3cppv34D","TMYDPECOlQ");
+
+// $conn = new mysqli("remotemysql.com","TMYDPECOlQ","6a3cppv34D","TMYDPECOlQ");
 
 if($conn){
   
 }
 
 else
-die("connection to this database failed due to " .mysqli_connect_error());
+    die("connection to this database failed due to " .mysqli_connect_error());
 
 
 ?>
